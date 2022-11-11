b6083ba8c66c238e8b9647f5f67c6a726efd531e
Tikhonov Ivan <ivntik77@gmail.com>
2022-11-11 11:44:32 +0600

    Адаптивность до 768 готова. Нет 1 блока форма обратной связи и отзовы

--- js/main.js
+++ js/main.js
@@ -27,3 +27,33 @@ $(document).ready(function () {
     ],
   });
 });
+
+$(document).ready(function () {
+  $(".reviews-cards").slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // speed: 1000,
+    // autoplaySpeed: 800,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 2,
+        },
+      },
+
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+        },
+      },
+    ],
+  });
+});
